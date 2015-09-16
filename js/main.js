3c32e1290dabc4a05c4264aceb30173288dfc205
Daniel Dyla <dyladan@gmail.com>
2015-09-16 10:25:30 -0400

    cache busting

--- js/main.js
+++ js/main.js
@@ -1,38 +1,40 @@
 window.onhashchange = function() {
-  render();
+    render();
 }
 
 function render() {
-  window.scrollTo(0,0);
-  $.getJSON("/config.json" + "#" + Date.now(), function(config) {
-    var url = window.location.hash, idx = url.indexOf("#");
-    var hash = idx != -1 ? url.substring(idx+1) : "";
-    document.title = config.title;
-    $(".navbar-brand")
-      .html(config.title);
-    if (hash === "") {
-      // render homepage
-      $("#content").html(
-        $("<div/>", {
-          id: "home",
-        })
-      );
-      $.get(config.index + "#" + Date.now(), function(data) {
-
-
-        $("#home")
-          .append($("<img/>", {
-            class: "feature",
-            src: "//www.gravatar.com/avatar/" + md5(config.email) + "?s=" + 200 * window.devicePixelRatio
-          }))
-          .append(marked(data));
-      });
-    } else {
-      $.get(hash, function(data) {
-        $("#content").html(marked(data));
-      });
-    }
-  });
+    window.scrollTo(0,0);
+    $.getJSON("/config.json", function(config) {
+        var url = window.location.hash, idx = url.indexOf("#");
+        var hash = idx != -1 ? url.substring(idx+1) : "";
+        document.title = config.title;
+        $(".navbar-brand").html(config.title);
+        if (hash === "") {
+            // render homepage
+            $("#content").html(
+                $("<div/>", {
+                    id: "home",
+                })
+            );
+            $.ajax({
+                url:config.index,
+                success: function(data) {
+                    $("#home")
+                    .append($("<img/>", {
+                        class: "feature",
+                        src: "//www.gravatar.com/avatar/" + md5(config.email) + "?s=" + 200 * window.devicePixelRatio
+                    }))
+                    .append(marked(data)); },
+                cache:false
+            });
+        } else {
+            $.ajax({
+                url:hash,
+                success: function(data) { $("#content").html(marked(data)); },
+                cache: false
+            });
+        }
+    });
 }
 
 
